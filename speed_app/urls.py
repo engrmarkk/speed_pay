96854cedbbffd82f8c14acfb4596958d473c56dc
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-08-04 11:31:02 +0100

    login and logout

--- speed_app/urls.py
+++ speed_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .endpoints.auth import CustomRegisterView
-from .endpoints.users import GetAllUsers
+from .endpoints.users import GetAllUsers, SendFunds, DepositFunds
+from dj_rest_auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/', CustomRegisterView.as_view(), name='register'),
     path('users/', GetAllUsers.as_view(), name='users'),
+    path('send-funds/', SendFunds.as_view(), name='send-funds'),
+    path('deposit-funds/', DepositFunds.as_view(), name='deposit-funds'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
