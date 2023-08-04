82e136a8d10ea5f637156e6594f76594afde3a6b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-08-04 13:12:29 +0100

    DISABLE COLLECTSTATIC

--- speed_proj/settings.py
+++ speed_proj/settings.py
@@ -168,3 +168,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DISABLE_COLLECTSTATIC=1
