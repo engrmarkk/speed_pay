ec54ec0fc0045fbb1ab392f0ba007dea5971cb0f
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2023-08-04 12:23:49 +0100

    permission class for registration view

--- speed_app/endpoints/auth.py
+++ speed_app/endpoints/auth.py
@@ -1,6 +1,8 @@
 from dj_rest_auth.registration.views import RegisterView
 from speed_app.serializers import CustomRegisterSerializer
+from rest_framework.permissions import AllowAny
 
 
 class CustomRegisterView(RegisterView):
+    permission_classes = [AllowAny]
     serializer_class = CustomRegisterSerializer
